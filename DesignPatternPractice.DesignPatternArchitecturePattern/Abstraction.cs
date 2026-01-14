d24c0d91be0d831f29df9854bb0b3e68e2ce413d
udaykumarburgula <udaykumar.burgula2k@gmail.com>
2026-01-15 01:16:02 +0530

    abstraction, encapsulation and polymorphism

---
+++ DesignPatternPractice.DesignPatternArchitecturePattern/Abstraction.cs
@@ -0,0 +1,81 @@
+﻿namespace DesignPatternPractice.DesignPatternArchitecturePattern
+{
+    public class Abstraction
+    {
+    }
+
+    /// <summary>
+    /// ICustomer
+    /// </summary>
+    public interface ICustomer 
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public decimal CalculateDiscount();
+    }
+
+    /// <summary>
+    /// IProduct
+    /// </summary>    
+    public interface IProduct
+    {
+        public string ProductName { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    /// <summary>
+    /// IDiscount
+    /// </summary>
+    public interface IDiscount
+    {
+        public decimal GetDiscountedPrice(ICustomer customer, IProduct product);
+    }
+
+
+    /// <summary>
+    /// IRating
+    /// </summary>
+    public interface IRating
+    {
+        decimal score { get; set; }
+
+        decimal GetRatingScore(ICustomer customer);
+    }
+
+    /***
+     * Default Access Modifier in C#
+     * Summary Table
+     * ****************************************
+        Context	             Default Access Modifier
+        Top-level class	     internal (Top-level types class, interface, struct, enum    
+            Nested class	 private
+        Top-level interface	 internal
+        Interface members	 public
+        Class fields	     private
+        Class methods	     private
+        Class properties	 private    
+     ****/
+
+
+    /*************************
+     * 
+     * **************
+     * Note:
+     * 1. Abstraction: Interfaces are used to define abstract types that specify a contract without implementation details.
+     * 2. Encapsulation: Interfaces help encapsulate behavior by exposing only the methods and properties defined in the interface, hiding the implementation details.
+     * 
+     * 🧠 Comparison Table
+        Relationship	Type	Ownership	Lifecycle Dependency
+Inheritance	IS-A	N/A	Child depends on Parent
+Association	USES-A	❌ None	Independent
+Aggregation	HAS-A	⚠️ Weak	Independent
+Composition	HAS-A	✅ Strong	Dependent
+     * 
+     ********************************************/
+
+
+}
---
+++ DesignPatternPractice.DesignPatternArchitecturePattern/Implementation.cs
@@ -0,0 +1,143 @@
+﻿namespace DesignPatternPractice.DesignPatternArchitecturePattern
+{
+    public class Implementation
+    {
+    }
+
+
+    /// <summary>
+    /// Customer
+    /// </summary>
+    public class NormalCustomer : ICustomer
+    {
+        public NormalCustomer(IRating rating) { 
+            
+            this.Rating = rating.GetRatingScore(this); // Association example: using IRating interface to get rating score
+            // connection between class NormalCustomer to interface IRating is very thin.
+        }
+
+        public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Age { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public decimal Amount { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        /// <summary>
+        /// Rating : give discount based on rating (cibil score)
+        /// </summary>
+        public decimal Rating { get; set; } 
+
+        public decimal CalculateDiscount()
+        {
+            if (IsValidAmount()) 
+            {
+
+                return 0;
+            }
+
+            return 0;
+        }
+
+        /// <summary>
+        /// private accessor to validate amount <- Encapsulation example
+        /// </summary>
+        /// <returns></returns>
+        private bool IsValidAmount()
+        {
+            if(Amount > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+
+    /// <summary>
+    /// StudentCustomer
+    /// </summary>
+    public class StudentCustomer : ICustomer
+    {
+        public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Age { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public decimal Amount { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        /// <summary>
+        /// InstitutionName : give discount based on institution name
+        /// </summary>
+        public string InstitutionName { get; set; } 
+        public decimal CalculateDiscount()
+        {
+            if (IsValidAmount())
+            {
+                return 0;
+            }
+            return 0;
+        }
+        /// <summary>
+        /// private accessor to validate amount <- Encapsulation example
+        /// </summary>
+        /// <returns></returns>
+        private bool IsValidAmount()
+        {
+            if (Amount > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+
+    /// <summary>
+    /// PremiumCustomer
+    /// </summary>
+    public class PremiumCustomer : ICustomer
+    {
+        public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int Age { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public decimal Amount { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        /// <summary>
+        /// MembershipYears : give discount based on membership years
+        /// </summary>
+        public int MembershipYears { get; set; } 
+        public decimal CalculateDiscount()
+        {
+            if (IsValidAmount())
+            {
+                return 0;
+            }
+            return 0;
+        }
+        /// <summary>
+        /// private accessor to validate amount <- Encapsulation example
+        /// </summary>
+        /// <returns></returns>
+        private bool IsValidAmount()
+        {
+            if (Amount > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// Product
+    /// </summary>
+    public class Product : IProduct
+    {
+        public string ProductName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public decimal Price { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+
+    /// <summary>
+    /// Discount
+    /// </summary>
+    public class Discount : IDiscount
+    {
+        public decimal GetDiscountedPrice(ICustomer customer, IProduct product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
